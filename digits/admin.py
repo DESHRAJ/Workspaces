3194764f19d946b83bfaa71efec39c6adcd17c55
Deshraj <deshrajdry@gmail.com>
2015-07-28 18:28:36 +0530

    Organizations admin modified

--- digits/admin.py
+++ digits/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+# from organizations.models import (Organization, OrganizationUser,
+#     OrganizationOwner)
+# from .forms import UserAdminForm
+# from .models import Account, AccountUser
 
-# Register your models here.
+# class AccountUserAdmin(admin.ModelAdmin):
+#     form = UserAdminForm()
+
+# admin.site.unregsiter(Organization)
+# admin.site.unregsiter(OrganizationUser)
+# admin.site.unregsiter(OrganizationOwner)
+# admin.site.register(Account)
+# admin.site.register(AccountUser, AccountUserAdmin)
