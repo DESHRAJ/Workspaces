967f3fbfcfd6323b49ea8837b522cf0e2e371649
Deshraj <deshrajdry@gmail.com>
2015-07-30 14:13:44 +0530

    Url added for dashboard view

--- Workspaces/urls.py
+++ Workspaces/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from organizations.backends import invitation_backend, registration_backend
-
+from digits import views
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'Workspaces.views.home', name='home'),
+    url(r'dashboard', 'digits.views.dashboard', name='dashboard'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^org/', include('organizations.urls')),
