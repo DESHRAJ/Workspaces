c4dd32b81b140ac654319416c84b03a65e79de04
Deshraj <deshrajdry@gmail.com>
2015-07-31 01:59:08 +0530

    models modified

--- digits/models.py
+++ digits/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 # Create your models here.
-from organizations.models import Organization, OrganizationUser
+from django.contrib.auth.models import User
+from organizations.models import OrganizationUser
 
-class Account(Organization):
-    class Meta:
-        proxy = True
+# class Account(Organization):
+#     class Meta:
+#         proxy = True
 
-class AccountUser(OrganizationUser):
-    class Meta:
-        proxy = True
+# class AccountUser(OrganizationUser):
+#     class Meta:
+#         proxy = True
+
+
+class Jobs(models.Model):
+	job_id = models.CharField(max_length = 10)
+	workspace = models.ForeignKey(OrganizationUser)
+
