dfc1dc880404257b8160a3779e03e25b6e7074bf
Deshraj <deshrajdry@gmail.com>
2015-08-18 19:13:51 +0530

    Urls modified

--- Workspaces/urls.py
+++ Workspaces/urls.py
@@ -1,15 +1,22 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from organizations.backends import invitation_backend, registration_backend
 from digits import views
+import os 
 urlpatterns = [
     # Examples:
+    url(r'^static/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static')}),
     url(r'^email/', 'digits.views.current_user', name='current_user'),
     url(r'dashboard', 'digits.views.dashboard', name='dashboard'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^org/', include('organizations.urls')),
+    url(r'^workspace/', include('organizations.urls')),
     url(r'^invitations/', include(invitation_backend().get_urls())),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('organizations.urls')),
 
 ]
+
+# urlpatterns += patterns('',
+#     (r'^static/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static')}),
+# )
